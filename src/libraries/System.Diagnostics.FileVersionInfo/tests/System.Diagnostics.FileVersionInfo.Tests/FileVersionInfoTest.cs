712c9b7656e98aef9b9d5aad337706f04fda290a
Milos Kotlar <kotlarmilos@gmail.com>
2026-04-15 16:29:47 +0200

    Refactor FileVersionInfoTest to simplify OriginalTestAssemblyInternalName initialization by removing Apple mobile condition

--- src/libraries/System.Diagnostics.FileVersionInfo/tests/System.Diagnostics.FileVersionInfo.Tests/FileVersionInfoTest.cs
+++ src/libraries/System.Diagnostics.FileVersionInfo/tests/System.Diagnostics.FileVersionInfo.Tests/FileVersionInfoTest.cs
@@ -11,7 +11,7 @@ namespace System.Diagnostics.Tests
     {
         private const string TestAssemblyFileName = "System.Diagnostics.FileVersionInfo.TestAssembly.dll";
         // On Unix the internal name's extension is .exe if OutputType is exe even though the TargetExt is .dll.
-        private readonly string OriginalTestAssemblyInternalName = PlatformDetection.IsWindows || PlatformDetection.IsAppleMobile ?
+        private readonly string OriginalTestAssemblyInternalName = PlatformDetection.IsWindows ?
             "System.Diagnostics.FileVersionInfo.TestAssembly.dll" :
             "System.Diagnostics.FileVersionInfo.TestAssembly.exe";
         private const string TestCsFileName = "Assembly1.cs";
